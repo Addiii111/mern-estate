68a8c37b29f9727488059fe58d917ba72b13945b
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-01-01 14:43:26 +0530

    create API route

---
+++ Server/controllers/test.js
@@ -0,0 +1,8 @@
+
+
+const test = (req, res) => {
+    res.send('i m Aditya')
+}
+
+
+module.exports = test
--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-
+const router = require("./routers/routes.js");
 require("./db/conn.js");
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(router);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
