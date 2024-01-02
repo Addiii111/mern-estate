dddfc023de85af77980028ab78bbc7980dd2c4e9
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-01-03 02:09:25 +0530

    complete signup page ui

--- client/src/Routes/routing.jsx
+++ client/src/Routes/routing.jsx
@@ -1,21 +1,21 @@
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import SignIn from '../pages/Signin'
-import SignOut from '../pages/SignOut'
 import Profile from '../pages/Profile'
 import Header from '../components/Header'
+import SignUp from '../pages/SignUp'
 
 export default function routing() {
   return (
     <>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path = '/' element={<Home/>}></Route>
-        <Route path = '/About' element={<About/>}></Route>
-        <Route path = '/sign-in' element={<SignIn/>}></Route>
-        <Route path = '/sign-out' element={<SignOut/>}></Route>
-        <Route path = '/profile' element={<Profile/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/About' element={<About />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
+        <Route path='/sign-up' element={<SignUp />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
       </Routes>
     </>
   )
