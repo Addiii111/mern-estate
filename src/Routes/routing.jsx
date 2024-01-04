cb45d81b7541d246f69a33a7b0f2df17fd9ab09c
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-01-04 23:49:19 +0530

    complete sign in page functionality

--- src/Routes/routing.jsx
+++ src/Routes/routing.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
-import SignIn from '../pages/Signin'
+import SignIn from '../pages/SignIn'
 import Profile from '../pages/Profile'
 import Header from '../components/Header'
 import SignUp from '../pages/SignUp'
