f29d2db6f3f28ec7e365485dfdee24eb36cfb2df
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-01-04 23:26:29 +0530

    Create sign in api route

---
+++ Server/controllers/signin.js
@@ -0,0 +1,26 @@
+const User = require('../models/user.js')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const signin = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email: email });
+        if (!validUser) return res.status(401).json({ error: 'User not found!' });
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return res.status(401).json({ error: 'Wrong Credentials!' });
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY)
+        const { password: pass, ...rest } = validUser._doc;
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+        }).json(rest)
+
+
+    } catch (error) {
+        res.status(401).json({ error: error });
+    }
+
+}
+
+module.exports = signin
--- Server/routers/routes.js
+++ Server/routers/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const test = require('../controllers/test.js');
 const signup = require('../controllers/signup.js');
+const signin = require('../controllers/signin.js');
 const router = new express.Router();
 
 router.get('/test', test)
 router.post('/api/signup', signup)
+router.post('/api/signin', signin)
 
 
 module.exports = router;
