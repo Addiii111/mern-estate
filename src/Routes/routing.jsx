dcbbbc7bf089803cae1aaff8954b0f59661dffeb
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-03-12 01:19:30 +0530

    Create search page UI

--- src/Routes/routing.jsx
+++ src/Routes/routing.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from '../components/PrivateRoute'
 import CreateListing from '../pages/CreateListing'
 import UpdateListing from '../pages/UpdateListing'
 import Listing from '../pages/Listing'
+import Search from '../pages/Search'
 
 export default function routing() {
   return (
@@ -19,6 +20,7 @@ export default function routing() {
         <Route path='/About' element={<About />}></Route>
         <Route path='/signin' element={<SignIn />}></Route>
         <Route path='/signup' element={<SignUp />}></Route>
+        <Route path='/search' element={<Search/>}></Route>
         <Route path='/listing/:listingId' element={<Listing />}></Route>
 
         <Route element={<PrivateRoute />}>
