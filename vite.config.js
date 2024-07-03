82f75e1d6e6d7a051334a818dfb51ded170969d5
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-07-03 23:39:06 +0530

    dsssds

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://mern-estate-mu.vercel.app',
-        secure: true,
+        secure: false,
       }
     }
   },
