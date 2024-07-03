a44fc0755b83566c106da58123a63c5aa4a063ea
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-07-03 23:04:39 +0530

    frontend deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://mern-estate-cjwbyky9j-addiii111s-projects.vercel.app',
-        secure: false,
+        secure: true,
       }
     }
   },
