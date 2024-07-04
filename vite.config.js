f623a5da2fd4b13aca39885d1e064372274d87dd
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-07-04 22:29:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://mern-estate-mu.vercel.app',
+        target: 'http://localhost:3001',
         secure: false,
       }
     }
