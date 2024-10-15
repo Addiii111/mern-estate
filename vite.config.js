c3b32da463f0d11500282496eec8958ef6635d9a
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-10-15 18:30:09 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api':'http://localhost:3001',
+      '/api':'https://mern-estate-34xz.onrender.com',
       
     }
   },
